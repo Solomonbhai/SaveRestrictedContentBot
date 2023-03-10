396448ada81951954ce2a2a84072a0a2e64f2a97
mohit-chauhan <mohitsinghchauhan12@gmail.com>
2023-03-10 11:15:46 +0530

    add var

--- main/__init__.py
+++ main/__init__.py
@@ -12,12 +12,12 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
                     level=logging.WARNING)
 
 # variables
-API_ID = config("API_ID", default=None, cast=int)
-API_HASH = config("API_HASH", default=None)
-BOT_TOKEN = config("BOT_TOKEN", default=None)
-SESSION = config("SESSION", default=None)
-FORCESUB = config("FORCESUB", default=None)
-AUTH = config("AUTH", default=None, cast=int)
+API_ID =  17809227
+API_HASH = '24c6288eee3e68db783afaf3df669534'
+BOT_TOKEN = '5105784060:AAEuQbnUpzwHslcL_cyoArS5F1Q2IVtN_Qk'
+SESSION = 'BQA50dDOyBFPh636_SkEnNvD99jiyG2Kr-plG5rl8FnHV7lQRH5y3eNCFrdj8DyuMkKt_j4Yln7OQ5by9CllgohCKeM4eNgfM2UzbVNi23fXNgPjnV3EZxcGyzIwm0qPdPBYo9tp5pd7vCUMc-uSaypocFbl7tsIGjChVVkyqitLD7Qv10wCCtlOgQCJQ9DaulPuQp-a0Hcv6dpFbod1pFPTTUMbo1GSWHJNv_LElkdDzsLjGy1DWWYx8ZgBqVtipTAoeMAV6xsvJDEjd22OGWa_pykc9p2TS2U_2RP_cBI-AjUD4udExQIAL2k6syPavgQrc2ozKwOR7Hja0hy0TD9eVFY_KAA'
+FORCESUB = 'ffffswwwwgggd'
+AUTH = 1414938408
 
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
 
